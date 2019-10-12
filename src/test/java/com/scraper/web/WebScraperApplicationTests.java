83196df6b3aa64811a2d09a96e445080bbdf875f
vmomot <momotvolodymyr@gmail.com>
2019-10-12 15:03:07 +0300

    Spring Boot initial commit

---
+++ src/test/java/com/scraper/web/WebScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scraper.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
